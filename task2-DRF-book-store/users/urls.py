30dbc0df230b11bca278a7edfa16d17e66da26c1
Ali Emad <90727865+aliemadabdo@users.noreply.github.com>
2026-08-22 19:17:46 +0300

    Add JWT token endpoints to user URLs

--- task2-DRF-book-store/users/urls.py
+++ task2-DRF-book-store/users/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import UserRegisterView, UserLoginView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="user-register"),
     path("login/", UserLoginView.as_view(), name="user-login"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
